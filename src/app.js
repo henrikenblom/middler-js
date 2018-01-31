5025675e66a8a513c3eb6e9be8ea7e0b183a30a3
Henrik Enblom <henrik@enblom.com>
2018-01-31 14:36:42 +0100

    First commit

---
+++ src/app.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express = require("express");
+class Server {
+    static bootstrap() {
+        return new Server();
+    }
+    constructor() {
+        this.app = express();
+    }
+}
