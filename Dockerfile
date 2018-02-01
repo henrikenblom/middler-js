6fc5bf1264065646c67219003a22b9e532a36090
Henrik Enblom <henrik@enblom.com>
2018-02-01 13:47:04 +0100

    Cleanup

--- Dockerfile
+++ Dockerfile
@@ -5,4 +5,4 @@ RUN npm install typescript tslint
 RUN npm install
 RUN npm run build
 EXPOSE 3000
-CMD [ "npm", "run", "serve" ]
+CMD [ "node", "dist/server.js" ]
--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,17 @@
 "use strict";
 
-import * as bodyParser from "body-parser";
 import * as express from "express";
 import * as compression from "compression";
-import * as expressValidator from "express-validator";
 import * as logger from "morgan";
-import * as lusca from "lusca";
 
 const app = express();
 app.set("port", process.env.PORT || 3000);
 
 app.use(compression());
 app.use(logger("dev"));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(expressValidator());
-app.use(lusca.xframe("SAMEORIGIN"));
-app.use(lusca.xssProtection(true));
+
+app.get("/", function (req, res) {
+    res.json("");
+});
 
 module.exports = app;
